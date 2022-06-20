122fb951fa2fafb98033bd10b16be6862020cf2f
wuwac <wuwachon44@gmail.com>
2022-06-20 21:43:25 +0800

    feat: modify login/register routes and add partial hbs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,19 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // user login
 router.get('/login', (req, res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {
-  res.redirect('/')
+router.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body
+    let errors = []
+    if (!email || !password) errors = [{message: '每個欄位都是必填'}]
+    const user = await User.findOne({ email, password }).lean()
+    if (!user && !errors.length) errors = [{message: '帳號或密碼不正確'}]
+    if (errors.length) return res.render('login', {
+      errors,
+      email,
+      password
+    })
+    res.redirect('/')
+  } catch(err) {
+    console.log(err)
+  }
+  
 })
 // user register
 router.get('/register', (req, res) => {
   res.render('register')
 })
-router.post('/register', (req, res) => {
-  res.redirect('/users/login')
+router.post('/register', async (req, res) => {
+  try {
+    const {name, email, password, confirmPassword} = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) errors.push({message: '每個欄位都是必填'})
+    if (password !== confirmPassword) errors.push({message: '密碼輸入不一致'})
+    const user = await User.findOne({ email })
+    if (user) errors.push({message: '此Email已被註冊過'})
+    if (errors.length) return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+    await User.create({ name, email, password })
+    res.redirect('/users/login')
+  } catch(err) {
+    console.log(err)
+  }
 })
 
 module.exports = router

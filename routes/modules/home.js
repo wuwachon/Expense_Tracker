e67f10b21e0a78145868514ab814559e73aed08a
wuwac <wuwachon44@gmail.com>
2022-06-21 13:12:19 +0800

    refactor: handle error message in each async function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,23 +5,27 @@ const Category = require('../../models/category')
 const Record = require('../../models/record.js')
 
 router.get('/', async (req, res) => {
-  const categoryId = req.query.category
-  const userId = req.user._id
-  const categorySelect = await Category.findById(categoryId).lean()
-  const title = categoryId? categorySelect.name : '類型'
-  const categories = await Category.find().lean().sort('id')
-  const recordsFind = categoryId? await Record.find({ categoryId, userId }).lean() : await Record.find({ userId }).lean()
-  const records = []
-  let totalAmount = 0
-  await Promise.all(
-    recordsFind.map(async record => {
-      const categoryFind = await Category.findById(record.categoryId).lean()
-      record.category = categoryFind.icon
-      records.push(record)
-      totalAmount += record.amount
-    })
-  )
-  res.render('index', {totalAmount, title, categories, records})
+  try {
+    const categoryId = req.query.category
+    const userId = req.user._id
+    const categorySelect = await Category.findById(categoryId).lean()
+    const title = categoryId? categorySelect.name : '類型'
+    const categories = await Category.find().lean().sort('id')
+    const recordsFind = categoryId? await Record.find({ categoryId, userId }).lean() : await Record.find({ userId }).lean()
+    const records = []
+    let totalAmount = 0
+    await Promise.all(
+      recordsFind.map(async record => {
+        const categoryFind = await Category.findById(record.categoryId).lean()
+        record.category = categoryFind.icon
+        records.push(record)
+        totalAmount += record.amount
+      })
+    )
+    res.render('index', {totalAmount, title, categories, records})
+  } catch(err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -6,37 +6,57 @@ const Record = require('../../models/record')
 
 // add new record
 router.get('/new', async (req, res) => {
-  const categories = await Category.find().lean().sort('id')
-  res.render('new', { categories })
+  try {
+    const categories = await Category.find().lean().sort('id')
+    res.render('new', { categories })
+  } catch(err) {
+    console.log(err)
+  }
 })
 router.post('/', async (req, res) => {
-  const record = req.body
-  record.userId = req.user._id
-  await Record.create(record)
-  res.redirect('/')
+  try {
+    const record = req.body
+    record.userId = req.user._id
+    await Record.create(record)
+    res.redirect('/')
+  } catch(err) {
+    console.log(err)
+  }
 })
 // edit a record
 router.get('/:id/edit', async (req, res) => {
-  const _id = req.params.id
-  const categories = await Category.find().lean().sort('id')
-  const record = await Record.findById(_id).lean()
-  const recordCategory = await Category.findById(record.categoryId).lean()
-  record.category = recordCategory.name
-  res.render('edit', {categories, record})
+  try {
+    const _id = req.params.id
+    const categories = await Category.find().lean().sort('id')
+    const record = await Record.findById(_id).lean()
+    const recordCategory = await Category.findById(record.categoryId).lean()
+    record.category = recordCategory.name
+    res.render('edit', {categories, record})
+  } catch(err) {
+    console.log(err)
+  }
 })
 router.put('/:id', async (req, res) => {
-  const _id = req.params.id
-  let record = await Record.findById(_id)
-  record = Object.assign(record, req.body)
-  await record.save()
-  res.redirect('/')
+  try {
+    const _id = req.params.id
+    let record = await Record.findById(_id)
+    record = Object.assign(record, req.body)
+    await record.save()
+    res.redirect('/')
+  } catch(err) {
+    console.log(err)
+  }
 })
 // delete a record
 router.delete('/:id/delete', async (req, res) => {
-  const _id = req.params.id
-  const record = await Record.findById(_id)
-  await record.remove()
-  res.redirect('/')
+  try {
+    const _id = req.params.id
+    const record = await Record.findById(_id)
+    await record.remove()
+    res.redirect('/')
+  } catch(err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
